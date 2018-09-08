5dd54558432c3986ca10f83bdff1f438e2d57860
Mingjie Li <mingjie.li@everbridge.com>
2018-09-08 18:41:29 +0800

    Update keymap

--- keyboards/xd60/keymaps/mj_mac/keymap.c
+++ keyboards/xd60/keymaps/mj_mac/keymap.c
@@ -1,7 +1,8 @@
-#include QMK_KEYBOARD_H
+ #include QMK_KEYBOARD_H
 
 // Helpful Defines
 #define _______    KC_TRNS
+#define XXXXXXX    KC_NO
 #define FN_CAPS    LT(1, KC_CAPS)
 
 // Mac Keyboard Shorts
@@ -27,15 +28,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_all(
           KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,  KC_GRV, KC_BSPC,
           KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,          KC_BSLS,
-         FN_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,   KC_NO,           KC_ENT,
-         KC_LSPO,   KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,   KC_NO, KC_RSPC,   MO(1),
+         FN_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, XXXXXXX,           KC_ENT,
+         KC_LSPO, XXXXXXX,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, XXXXXXX, KC_RSPC,   MO(1),
          KC_LCTL, KC_LALT, KC_LGUI,                             KC_SPC,                            KC_RGUI, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT),
     // Function Lay
     [1] = LAYOUT_all(
         KC_POWER,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  KC_DEL,
-         _______, _______, SCR2DSK,  FINDER, _______, _______, _______, KC_PGUP, KC_PGDN, _______, KC_MPLY, KC_MRWD, KC_MFFD,            RESET,
-         _______,   KC_F4, SCR2CLP,  KC_F11, FULLSCR, _______, KC_HOME,  KC_END, _______, LOCKSCR,  KC_F14,  KC_F15,   KC_NO,          KC_BTN1,
-         _______, _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, _______,   KC_NO, _______, _______,
+         _______, _______, SCR2DSK, _______, _______, _______, KC_PGUP, KC_HOME,   KC_UP,  KC_END, KC_MPLY, KC_MRWD, KC_MFFD,            RESET,
+         _______,   KC_F4, SCR2CLP,  KC_F11, _______, _______, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT,  KC_F14,  KC_F15, XXXXXXX,          _______,
+         _______, XXXXXXX, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_BTN1, XXXXXXX, KC_BTN2, _______,
          _______, _______, _______,                            _______,                            _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R)
 };
 
